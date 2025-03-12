4d178d23f05553214d8395f5a8db1b6901385ce7
Jake Schlottag <jschlottag.1@gmail.com>
2025-03-12 09:13:47 -0400

    update config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
                     "lua_ls",
                     "ansiblels",
                     "bashls",
@@ -24,36 +24,48 @@ return {
                     "sqlls",
                     "tflint",
                     "harper_ls",
-                    "gitlab_ci_ls"
-            },
-        })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ansiblels.setup({})
-      lspconfig.bashls.setup({})
-      lspconfig.tailwindcss.setup({})
-      lspconfig.dockerls.setup({})
-      lspconfig.helm_ls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.ts_ls.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.grammarly.setup({})
-      lspconfig.jedi_language_server.setup({})
-      lspconfig.sqlls.setup({})
-      lspconfig.tflint.setup({})
-      lspconfig.harper_ls.setup({})
-      lspconfig.gitlab_ci_ls.setup({})
+                    "gitlab_ci_ls",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = { 'saghen/blink.cmp' },
 
+        opts = {
+            servers = {
+                lua_ls = {},
+                ansiblels = {},
+                bashls = {},
+                tailwindcss = {},
+                dockerls = {},
+                helm_ls = {},
+                html = {},
+                ts_ls = {},
+                jsonls = {},
+                grammarly = {},
+                jedi_language_server = {},
+                sqlls = {},
+                tflint = {},
+                harper_ls = {},
+                gitlab_ci_ls = {}
+            }
+        },
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
-};
+        config = function(_, opts)
+            local lspconfig = require("lspconfig")
+            local cmp_capabilities = require('blink.cmp').get_lsp_capabilities()
+
+            for server, config in pairs(opts.servers) do
+                config.capabilities = vim.tbl_deep_extend("force", cmp_capabilities, config.capabilities or {})
+                lspconfig[server].setup(config)
+            end
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
+}
--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,7 +7,6 @@ return {
     'MunifTanjim/nui.nvim'
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
-    vim.api.nvim_set_keymap('n', '<leader>e', ':Neotree focus<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>e', ':Neotree filesystem toggle reveal left<CR>')
   end
 }
