fc186dfa9d85539b5f83a1f4df2fa15c28b0847a
Jake Schlottag <jschlottag.1@gmail.com>
2025-03-12 23:27:35 -0400

    added snippets, completions, lualine, and harpoon. added some global npm packages in run

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,71 +1,61 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "ansiblels",
-                    "bashls",
-                    "tailwindcss",
-                    "dockerls",
-                    "helm_ls",
-                    "html",
-                    "ts_ls",
-                    "jsonls",
-                    "grammarly",
-                    "jedi_language_server",
-                    "sqlls",
-                    "tflint",
-                    "harper_ls",
-                    "gitlab_ci_ls",
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = { 'saghen/blink.cmp' },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ansiblels",
+					"bashls",
+					"tailwindcss",
+					"dockerls",
+					"helm_ls",
+					"html",
+					"ts_ls",
+					"jsonls",
+					"grammarly",
+					"jedi_language_server",
+					"sqlls",
+					"tflint",
+					"harper_ls",
+					"gitlab_ci_ls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        opts = {
-            servers = {
-                lua_ls = {},
-                ansiblels = {},
-                bashls = {},
-                tailwindcss = {},
-                dockerls = {},
-                helm_ls = {},
-                html = {},
-                ts_ls = {},
-                jsonls = {},
-                grammarly = {},
-                jedi_language_server = {},
-                sqlls = {},
-                tflint = {},
-                harper_ls = {},
-                gitlab_ci_ls = {}
-            }
-        },
+			local lspconfig = require("lspconfig")
 
-        config = function(_, opts)
-            local lspconfig = require("lspconfig")
-            local cmp_capabilities = require('blink.cmp').get_lsp_capabilities()
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.ansiblels.setup({ capabilities = capabilities })
+			lspconfig.bashls.setup({ capabilities = capabilities })
+			lspconfig.tailwindcss.setup({ capabilities = capabilities })
+			lspconfig.dockerls.setup({ capabilities = capabilities })
+			lspconfig.helm_ls.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities })
+			lspconfig.ts_ls.setup({ capabilities = capabilities })
+			lspconfig.jsonls.setup({ capabilities = capabilities })
+			lspconfig.grammarly.setup({ capabilities = capabilities })
+			lspconfig.jedi_language_server.setup({ capabilities = capabilities })
+			lspconfig.sqlls.setup({ capabilities = capabilities })
+			lspconfig.tflint.setup({ capabilities = capabilities })
+			lspconfig.harper_ls.setup({ capabilities = capabilities })
+			lspconfig.gitlab_ci_ls.setup({ capabilities = capabilities })
 
-            for server, config in pairs(opts.servers) do
-                config.capabilities = vim.tbl_deep_extend("force", cmp_capabilities, config.capabilities or {})
-                lspconfig[server].setup(config)
-            end
-
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
